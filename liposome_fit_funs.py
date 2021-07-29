79c0958addb5be63e976084c95f467b84973cc44
Laurence Lurio <llurio@niu.edu>
2021-07-29 16:42:20 -0500

    Commit after modifying documentation and changing structure of files a bit.  The actual fit is run as a subroutine and seperated out from run_liposome_fit.

---
+++ liposome_fit_funs.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 09:48:33 2021
+
+@author: lluri
+"""
+from matplotlib import pyplot as plt
+from liposome_saxs_funs import  profile
+from liposome_model import liposome_model
+from liposome_sum_code import shorten
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+import os
+def fit_liposome(data,par,fit_pars):
+    #%% (D) loop through each data set and fit
+    multi_results_out = {}
+    ngood = 0
+    water = shorten(data[fit_pars['backgrounds'][0]],
+                    fit_pars['nshort'])
+    air = shorten(data[fit_pars['backgrounds'][1]],
+                  fit_pars['nshort'])
+    for dind, dsetname in enumerate(data.keys()):
+        selection = (dsetname in fit_pars['selected_data'])
+        if (dsetname not in fit_pars['backgrounds'] and 
+            selection):
+            ngood += 1
+            fitset = shorten(data[dsetname],fit_pars['nshort'])
+            par['R0'].value = fit_pars['aux_data'][dsetname]['R']
+            par['Rsig'].value = fit_pars['aux_data'][dsetname]['dR']
+            q = fitset['q']
+            I = fitset['I']
+            w = np.zeros(len(fitset['dI']))            
+            rr = (q>fit_pars['qmin'])*(q<fit_pars['qmax'])
+            sys_err = 0 # estimate of systematica normalization error
+            w[rr] = 1/np.sqrt(fitset['dI'][rr]**2 + water['dI'][rr]**2 +
+                              (sys_err*fitset['I'][rr])**2 +(sys_err*water['I'][rr])**2)
+            w[1^rr] = 0 # "eps" is small but nonzero
+            # choose fit method, least squares = 1, differential evolution =0
+            if (fit_pars['fit_method']):
+                print('running least squares fit')
+                result = liposome_model.fit(I,par,q=q,bgfun1 = water['I'],
+                            bgfun2=air['I'],weights=w)
+            else:
+                print('running differential evolution fit ')
+                result = liposome_model.fit(I,par,q=q,bgfun1 = water['I'],
+                            bgfun2=air['I'],weights=w,
+                            method='differential_evolution',max_nfev=fit_pars['nfev'],
+                            fit_kws={'popsize':fit_pars['psize']}
+                            )
+                # append additional variables to result values
+                result.values['fit_pars'] = fit_pars
+                print('Finished fit #2 redchi = {0:7.2f}'.format(result.redchi))
+                multi_results_out[dsetname] = {'result':result,
+                    'cfrac':fit_pars['aux_data'][dsetname]['cfrac'],'dsetname':dsetname}
+            print('Fit to dataset {0:s} complete'.format(dsetname))
+    with open(fit_pars['resultname'],'wb') as fd:
+        np.save(fd,multi_results_out,allow_pickle=True)
+    return result
+def make_plots(result,fit_pars,dsetname):
+    pp = PdfPages(fit_pars['figname'])
+    #
+    # First figure, printout of fit report
+    #
+    if not os.path.exists('Results'):
+        os.mkdir('Results')
+    fit_results = result.fit_report()
+    fig = plt.figure('results')
+    fig.clf()
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.text(.02,.02,fit_results,fontsize='x-small')
+    ax.text(.75,.9,dsetname,fontsize='large')
+    ax.axis('off')
+    #fig.show()
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    #
+    # Second figure plot data and background
+    #
+    vals = result.values
+    q = result.userkws['q']
+    bgfun1 = result.userkws['bgfun1']
+    bgfun2 = result.userkws['bgfun2']
+    bg = (vals['bg'] + q*vals['lbg'] +
+        q**2*vals['qbg'] + 
+        bgfun1*vals['bg1sf'] +
+        bgfun2*vals['bg2sf'])
+    data = result.data
+    fit = result.best_fit
+    plt.figure('Fit_with_bg')
+    plt.clf()
+    w = result.weights>0
+    plt.errorbar(q[w],data[w],1/result.weights[w],fmt='-k',label='data')
+    plt.plot(q[w],fit[w],'-r',label='fit')
+    #plt.plot(q[w], bgfun1[w]*vals['bg1sf'],'-b',label='water')
+    #plt.plot(q[w], bgfun2[w]*vals['bg2sf'],'-g',label='glass')
+    bg = (vals['bg'] + q*vals['lbg'] +
+     q**2*vals['qbg'] + 
+     bgfun1*vals['bg1sf'] +
+     bgfun2*vals['bg2sf'])
+    plt.plot(q[w], bg[w],'--k',label='total bg')
+    plt.yscale('linear')
+    plt.xscale('linear')
+    plt.ylim(.0325,.07)
+    plt.title(dsetname) 
+    plt.xlabel('q (nm$^{-1}$)')
+    plt.ylabel(r'$\frac{1}{V}\frac{d\Sigma}{d\Omega}$ (cm $^{-1}$)')
+    plt.legend()
+    #plt.show()
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    #
+    # Third figure background subtracted data
+    #
+    vals = result.values
+    q = result.userkws['q']
+    bgfun1 = result.userkws['bgfun1']
+    bgfun2 = result.userkws['bgfun2']
+    bg = (vals['bg'] + q*vals['lbg'] +
+        q**2*vals['qbg'] + 
+        bgfun1*vals['bg1sf'] +
+        bgfun2*vals['bg2sf'])
+    bgdata = result.data-bg
+    qfull = np.linspace(0.05,7,10000)
+    # create data set of fit over finer q-range without background.
+    fit = liposome_model.eval(result.params,q=qfull,bgfun1 = qfull*0,bgfun2=qfull*0)
+    #fit = result.best_fit - bg
+    plt.figure('Fit_minus_bg')
+    plt.clf()
+    #plt.errorbar(q[w],bgdata[w],1/result.weights[w],fmt='-k',label='data')
+    plt.plot(q[w],bgdata[w],'ko',label='data',markersize=2)
+    #plt.plot(q[w],fit[w],'-r',label='fit')
+    plt.plot(qfull,fit,'-r',label='fit')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.ylim(1e-6,1)
+    plt.title(dsetname) 
+    plt.xlabel('q (nm$^{-1}$)')
+    plt.ylabel(r'$\frac{1}{V}\frac{d\Sigma}{d\Omega}$ (cm $^{-1}$)')
+    plt.legend()
+    #plt.show()
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    # #%%
+    # Fourth figure, plot residuals
+    plt.figure('Fractonal Residuals')
+    plt.clf()
+    plt.errorbar(q[w],(result.data[w]-result.best_fit[w])/result.data[w],
+        1/result.weights[w]/result.data[w],fmt='ks',label='data')
+    plt.yscale('linear')
+    plt.ylim(-.025,.025)
+    plt.title(dsetname)
+    plt.xlabel('q (nm$^{-1}$)')
+    plt.ylabel('residuals')
+    plt.legend()
+    #plt.show()
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    # #%%
+    # Fifth figure plot the profile in real space corresponding to the fit
+    plt.figure('Real_Space_Fit')
+    plt.clf()
+    P3 = profile([])
+    P3.load_par(result.params)
+    P3.draw_rho(offset = 333.3, ymin = 0, ymax = 450, color='blue')
+    plt.title(dsetname)
+    #plt.show()
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    pp.close()
+    #%%
+def key_plot(Cf,y,dy,Rs,chi2,rlim,chi2lim):
+    gg1 = (chi2<chi2lim)*(Rs<rlim)
+    gg2 = (chi2>chi2lim)*(Rs<rlim)
+    gg3 = (chi2<chi2lim)*(Rs>rlim)
+    gg4 = (chi2>chi2lim)*(Rs>rlim)
+    if (sum(gg1)>0):
+        plt.errorbar(Cf[gg1],y[gg1],dy[gg1],fmt='ks',
+                label='R < {0:d} chi2 < {1:d}'.format(rlim,chi2lim))
+    if (sum(gg2)>0):
+        plt.errorbar(Cf[gg2],y[gg2],dy[gg2],fmt='ko',
+                label='R < {0:d} chi2 > {1:d}'.format(rlim,chi2lim))    
+    if (sum(gg3)>0):
+        plt.errorbar(Cf[gg3],y[gg3],dy[gg3],fmt='rs',
+                label='R > {0:d} chi2 < {1:d}'.format(rlim,chi2lim))  
+    if (sum(gg4)>0):
+        plt.errorbar(Cf[gg4],y[gg4],dy[gg4],fmt='ro',
+                label='R > {0:d} chi2 > {1:d}'.format(rlim,chi2lim))
+def plot_final_results(result,fit_pars):
+    # reload mult-results file and restore
+    with open(fit_pars['resultname'],'rb') as fd:
+        multi_results_in = np.load(fd,allow_pickle=True)[()]
+    pp = PdfPages(fit_pars['figname'])
+    #%%
+    # collect summary results
+    Rs = np.array([])
+    sigs = np.array([])
+    dsigs = np.array([])
+    Ws = np.array([])
+    dWs = np.array([])
+    chi2 = np.array([])
+    Cf = np.array([])
+    W_asym = np.array([])
+    dW_asym = np.array([])
+    A_T_asym = np.array([])
+    dA_T_asym = np.array([])
+
+    col = ['black','red','green','blue','cyan','magenta']
+    lsty = ['solid','dashed','dashdot','dotted']
+    ncol = len(col)
+    for nout, tout in enumerate(multi_results_in):
+        tres = multi_results_in[tout]['result']
+        tsetname = multi_results_in[tout]['dsetname']
+        make_plots(result,fit_pars,tsetname)
+    plt.figure('real_space_plots_overview')
+    plt.clf()
+    for nout, tout in enumerate(multi_results_in):
+        tres = multi_results_in[tout]['result']
+        tsetname = multi_results_in[tout]['dsetname']
+        cfrac = multi_results_in[tout]['cfrac']
+        params = tres.params  
+        chi2 = np.append(chi2,tres.redchi)
+        sigs = np.append(sigs,params['sig'].value)
+        Rs = np.append(Rs,params['R0'].value)
+        Ws = np.append(Ws,params['W'].value)
+        dsigs = np.append(dsigs,params['sig'].stderr)
+        dWs = np.append(dWs,params['W'].stderr)
+        Cf  = np.append(Cf,cfrac)
+        W_asym = np.append(W_asym,params['W_asym'].value)
+        dW_asym = np.append(dW_asym,params['W_asym'].stderr)
+        A_T_asym = np.append(A_T_asym,params['A_T_asym'].value)
+        dA_T_asym = np.append(dA_T_asym,params['A_T_asym'].stderr)
+        P3 = profile([])
+        P3.load_par(params)
+        off = cfrac*200
+        P3.draw_rho(offset = 333.3+off, ymin = 300, 
+                    ymax = 525, color=col[nout%ncol],
+                    linestyle = lsty[int(nout/ncol)],label=tsetname)
+    plt.xlabel('distance [nm]')
+    plt.ylabel('electron density $e^-/$nm$^3$')
+    plt.legend(loc=(1.04,0))
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    plt.figure('Ws')
+    plt.clf()
+    chi2lim = 2
+    rlim = 100
+    key_plot(Cf,Ws,dWs,Rs,chi2,rlim,chi2lim)
+    plt.xlabel('cholesterol fraction')
+    plt.ylabel('bilayer thickness')
+    plt.legend(loc=(1.04,0))
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    
+    
+    #plt.savefig('Results\\'+timestring+'thickness.png')
+    plt.figure('sigs')
+    plt.clf()
+    key_plot(Cf,sigs,dsigs,Rs,chi2,rlim,chi2lim)
+    plt.xlabel('cholesterol fraction')
+    plt.ylabel('bilayer roughness')
+    plt.legend(loc=(1.04,0))
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    #plt.savefig('Results\\'+timestring+'roughness.png')
+    
+    plt.figure('chi2')
+    plt.clf()
+    gg1 = Rs<rlim
+    gg2 = Rs>rlim
+    if sum(gg1)>1:
+        plt.plot(Cf[gg1],chi2[gg1],'ks',label='R0 < {0:4.1f}'.format(rlim))
+    if sum(gg2)>1:
+        plt.plot(Cf[gg2],chi2[gg2],'rs',label='R0 > {0:4.1f}'.format(rlim))
+    plt.xlabel('cholesterol fraction')
+    plt.ylabel('fit chi-squared (reduced) ')
+    plt.legend(loc=(1.04,0))
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+    #plt.savefig('Results\\'+timestring+'redchi.png')
+    
+    plt.figure('asymmetry')
+    plt.clf()
+    key_plot(Cf,W_asym,dW_asym,Rs,chi2,rlim,chi2lim)
+    plt.legend()
+    plt.xlabel('cholesterol fraction')
+    plt.ylabel('fit asymmetry parameter ')
+    plt.legend(loc=(1.04,0))
+    plt.savefig(pp, format='pdf',bbox_inches='tight')
+
+    
--- run_liposome_fit.py
+++ run_liposome_fit.py
@@ -9,17 +9,11 @@ a single file to run replaced by running all the files in a loop
 @author: Laurence Lurio
 
 """
-
-from liposome_saxs_funs import  liposome_model
-from liposome_sum_code import shorten
 import numpy as np
 from lmfit import Parameters
-import os
 import time
-from liposome_plotting import plot_final_results
-from matplotlib.backends.backend_pdf import PdfPages
-
-#%%
+from liposome_fit_funs import fit_liposome, plot_final_results
+#%% (A)
 # read in saved data and open
 dataname =  'liposome_data.npy'
 with open(dataname,'rb') as fdir:
@@ -41,26 +35,26 @@ aux_data = {'egg4_chol1_200_a':{'cfrac':0.2,'R':200/2,'dR':60/2},
             }
 #%%
 # Define list of just 50 (62) nm diameter 
+fit_pars = {}
+fit_pars['aux_data'] = aux_data
 D50 = ['eggPC_50_d','egg4_chol1_50_a','egg2_chol3_50_b',
       'egg1_chol1_50_a','egg3_chol2_50_a','egg2_chol1_50_a']
-#%% Set up fit directory and names
-if not os.path.exists('Results'):
-    os.mkdir('Results')
+fit_pars['selected_data'] = D50[0:2]
+fit_pars['backgrounds'] = ['water_c','aircap_c']
+#%% Set up  names (B)
 T1 = time.time()
 timestring = str(int(T1))[-6:]
-figname = 'Results/liposome_fit_results_'+timestring+'.pdf'
-resultname = 'Results/liposome_fit_results_'+timestring+'.npz'
-pp = PdfPages(figname)
-multi_results_out = {}
-#%% Run through data and fit
-select_all = 0  # flag to select all datasets or just a subset
-nshort = 4      # number of times to average data set by 1/2
-# Truncate the data range so as not to fit below qmin and above qmax
-qmin = 0.1
-qmax = 5
-eps = 1e-3
-water = shorten(data['water_c'],nshort)
-air = shorten(data['aircap_c'],nshort)
+fit_pars['figname'] = 'Results/liposome_fit_results_'+timestring+'.pdf'
+fit_pars['resultname'] = 'Results/liposome_fit_results_'+timestring+'.npz'
+#%% (C) Define parameters for fit
+fit_pars['nshort'] = 4      
+fit_pars['qmin'] = 0.1
+fit_pars['qmax'] = 5
+fit_pars['nfev'] =200000
+fit_pars['psize'] = 16
+linear_regression = 1
+differential_evolution = 0
+fit_pars['fit_method'] = differential_evolution
 #%%
 #Initialize the parameters for the fit.
 #
@@ -82,61 +76,9 @@ par.add('R0',value=50,vary=False,min=10,max=300)
 par.add('Rsig',value=15,vary=False,min=.25*10,max=.25*300) 
 par.add('W_asym',value=0,vary=False,min=-2,max=2) 
 par.add('A_T_asym',value=0,vary=False,min=-2,max=2) 
-#%%
-# loop through fit
-ngood = 0
-for dind, dsetname in enumerate(data.keys()):
-    if select_all:
-        selection = 1 # select all sets
-        nsets = len(data.keys)-2
-    else:
-        selection = (dsetname in D50)
-        nsets = len(D50)
-    if (dsetname not in ['water_c','aircap_c'] and selection):
-        ngood += 1
-        print('dataset {0:20s}\t (set number {1}) {2} out of {3}'.format(dsetname,
-        dind,ngood,nsets))
-        fitset = shorten(data[dsetname],nshort)
-        par['R0'].value = aux_data[dsetname]['R']
-        par['Rsig'].value = aux_data[dsetname]['dR']
-        q = fitset['q']
-        I = fitset['I']
-        w = np.zeros(len(fitset['dI']))            
-        rr = (q>qmin)*(q<qmax)
-        sys_err = 0 # estimate of systematica normalization error
-        w[rr] = 1/np.sqrt(fitset['dI'][rr]**2 + water['dI'][rr]**2 +
-                          (sys_err*fitset['I'][rr])**2 +(sys_err*water['I'][rr])**2)
-        w[1^rr] = 0 # "eps" is small but nonzero
-        # choose fit method, least squares = 1, differential evolution =0
-        fit_method = 0
-        if (fit_method):
-            print('running least squares fit')
-            result = liposome_model.fit(I,par,q=q,bgfun1 = water['I'],
-                        bgfun2=air['I'],weights=w)
-        else:
-            psize = 16
-            print('running differential evolution fit ')
-            result = liposome_model.fit(I,par,q=q,bgfun1 = water['I'],
-                        bgfun2=air['I'],weights=w,
-                        method='differential_evolution',max_nfev=200000,
-                        fit_kws={'popsize':psize}
-                        )
-            # append additional variables to result values
-            result.values['cfrac'] = aux_data[dsetname]['cfrac']
-            result.values['psize'] = psize
-            result.values['qmin'] = qmin
-            result.values['qmax'] = qmax
-            print('Finished fit #2 redchi = {0:7.2f}'.format(result.redchi))
-            multi_results_out[dsetname] = {'result':result,
-                'cfrac':aux_data[dsetname]['cfrac'],'dsetname':dsetname}
-        #make_plots(pp,result,dsetname)
-        print('Fit to dataset {0:s} complete'.format(dsetname))
-timestring = str(int(time.time()))[-6:]
-multi_name = 'liposome_multi_fit_results'+timestring+'.npy'
-with open(resultname,'wb') as fd:
-    np.save(fd,multi_results_out,allow_pickle=True)
-plot_final_results(pp,resultname)
-pp.close()
-T2 = time.time()
-print('total elapsed time {0:4.1f} s'.format(T2-T1))
+#%% Now run the actual fit
+result = fit_liposome(data,par,fit_pars)  
+#%% Plot the fit results
+plot_final_results(result,fit_pars)
+
 
